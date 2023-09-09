bdc795ef53f5b878a0775aeccb8e3cbb55295fa1
shiyukina01161125 <akama.techcamp@gmail.com>
2023-09-09 12:16:29 +0900

    コントローラー及びモデルの記述

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,2 +1,8 @@
 class Prototype < ApplicationRecord
+  belongs_to :user
+  
+  validates :prototype_name, presence: true
+  validates :catchphrase, presence: true
+  validates :concept, presence: true
+  validates :image, presence: true
 end
